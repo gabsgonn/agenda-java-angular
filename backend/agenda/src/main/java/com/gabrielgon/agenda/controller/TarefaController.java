075a55b2db43305078a93e4a99f71c7a722ead87
gabsgonn <gab.gd23@gmail.com>
2026-06-08 23:49:33 -0300

    Criando controller Tarefa pronto para GET, POST, PUT e DELETE

---
+++ backend/agenda/src/main/java/com/gabrielgon/agenda/controller/TarefaController.java
@@ -0,0 +1,49 @@
+package com.gabrielgon.agenda.controller;
+
+import com.gabrielgon.agenda.domain.Tarefa;
+import com.gabrielgon.agenda.service.TarefaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TarefaController {
+
+    private final TarefaService service;
+
+    public TarefaController(TarefaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Tarefa create(@RequestBody Tarefa tarefa) {
+        return service.save(tarefa);
+    }
+
+    @GetMapping
+    public List<Tarefa> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> getById(@PathVariable Long id) {
+        return service.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public Tarefa update(@PathVariable Long id,@RequestBody Tarefa tarefa) {
+        return service.update(id, tarefa);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
