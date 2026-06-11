39e0bf17052968f89cfa2f95016f19ac5ebf5b2a
gabsgonn <gab.gd23@gmail.com>
2026-06-11 00:39:18 -0300

    Corrigindo obter lista de tarefas

--- frontend/agenda/src/app/core/services/tarefa.service.ts
+++ frontend/agenda/src/app/core/services/tarefa.service.ts
@@ -2,14 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Tarefa } from '../../models/tarefa-model';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TarefaService {
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Tarefa[]> {
-    return this.http.get<Tarefa[]>('/api/tasks');
+    return this.http.get<Tarefa[]>(`${this.apiUrl}/api/tasks`);
   }
 }
