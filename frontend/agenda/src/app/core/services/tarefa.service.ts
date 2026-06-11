da8d81edfa559e5760c1b19ef43140cbe8115c6b
gabsgonn <gab.gd23@gmail.com>
2026-06-11 00:31:13 -0300

    Serviço Tarefa obter lista de todas as tarefas

--- frontend/agenda/src/app/core/services/tarefa.service.ts
+++ frontend/agenda/src/app/core/services/tarefa.service.ts
@@ -1,6 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Tarefa } from '../../models/tarefa-model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TarefaService {}
+export class TarefaService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Tarefa[]> {
+    return this.http.get<Tarefa[]>('/api/tasks');
+  }
+}
