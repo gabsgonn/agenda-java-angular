d0317b6d8f4159f4b050c3c62ab9e41caf482a72
gabsgonn <gab.gd23@gmail.com>
2026-06-20 12:45:30 -0300

    Fazendo os primeiros GET no front-end

--- frontend/agenda/src/app/pages/tarefas/tarefa-list/tarefa-list.ts
+++ frontend/agenda/src/app/pages/tarefas/tarefa-list/tarefa-list.ts
@@ -1,18 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormArray, ReactiveFormsModule } from '@angular/forms';
+import { Observable, tap } from 'rxjs';
+
 import { Tarefa } from '../../../models/tarefa-model';
 import { TarefaService } from '../../../core/services/tarefa.service';
-import { CommonModule } from '@angular/common';
-import {
-  FormBuilder,
-  FormGroup,
-  FormArray,
-  ReactiveFormsModule,
-  Validators,
-  Form,
-} from '@angular/forms';
 import { StatusTarefaEnum } from '../../../shared/enums/status-tarefa.enum';
-import { error } from 'node:console';
-import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'scss-tarefa-list',
@@ -23,6 +16,7 @@ import { forkJoin } from 'rxjs';
 })
 export class TarefaList implements OnInit {
   form!: FormGroup;
+  tarefas$!: Observable<Tarefa[]>;
 
   opcoesStatus = Object.entries(StatusTarefaEnum).map(([chave, valor]) => ({
     chave,
@@ -35,37 +29,25 @@ export class TarefaList implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // 1. Inicializa o formulário principal contendo um FormArray vazio
     this.form = this.fb.group({
       tarefas: this.fb.array([]),
     });
 
-    this.carregarTarefas();
+    this.tarefas$ = this.tarefaService.obterTarefas().pipe(
+      tap((dados) => {
+        this.tarefasFormArray.clear();
+
+        dados.forEach((tarefa) => {
+          this.tarefasFormArray.push(this.criarGrupoTarefa(tarefa));
+        });
+      }),
+    );
   }
 
-  // Getter para facilitar o acesso ao FormArray no HTML e no TS
   get tarefasFormArray(): FormArray {
     return this.form.get('tarefas') as FormArray;
   }
 
-  carregarTarefas(): void {
-    this.tarefaService.obterTarefas().subscribe({
-      next: (dados: Tarefa[]) => {
-        // 2. Limpa o array caso já existam dados
-        this.tarefasFormArray.clear();
-
-        // 3. Para cada tarefa recebida da API, cria um grupo de formulário e insere no FormArray
-        dados.forEach((tarefa) => {
-          this.tarefasFormArray.push(this.criarGrupoTarefa(tarefa));
-        });
-      },
-      error: (erro) => {
-        console.error('Erro ao buscar tarefas:', erro);
-      },
-    });
-  }
-
-  // Método auxiliar para transformar o modelo da Tarefa em controles do formulário
   private criarGrupoTarefa(tarefa: Tarefa): FormGroup {
     return this.fb.group({
       id: [tarefa.id],
@@ -80,14 +62,13 @@ export class TarefaList implements OnInit {
 
   // === Patch status ===
   updateStatusDaTarefa(index: number) {
-    const agrupar = this.tarefasFormArray.at(index);
-    const id = agrupar.get('id')?.value;
-    const status = agrupar.get('status')?.value;
-
+    const grupo = this.tarefasFormArray.at(index);
+    const { id, status } = grupo.value;
     this.tarefaService.updateStatus(id, status).subscribe({
       next: () => alert('Status da tarefa atualizado com sucesso!'),
       error: (err) => {
-        (console.log(err), alert('Não foi possível atualizar o status.'));
+        console.error(err);
+        alert('Não foi possível atualizar o status.');
       },
     });
   }
