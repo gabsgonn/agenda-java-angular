fcc1b744d036241bbe00bd8b961b4304f2b85aed
gabriel.goncalves <gab.gd23@gmail.com>
2026-07-29 11:39:20 -0300

    feat: Adicionando novos icones

--- frontend/agenda/src/app/shared/icons/raw-icons.ts
+++ frontend/agenda/src/app/shared/icons/raw-icons.ts
@@ -1,11 +1,11 @@
 import { tarefaIcon } from './tarefas-list.icon';
 import { contatoIcon } from './contatos-list.icon';
-// import { configuracaoIcon } from './configuracao.icon';
-// import { logoutIcon } from './logout.icon';
+import { configuracaoIcon } from './configuracao.icon';
+import { logoutIcon } from './logout.icon';
 
 export const RawIcons = {
   tarefas: tarefaIcon,
   contatos: contatoIcon,
-//   configuracoes: configuracaoIcon,
-//   logout: logoutIcon,
+  configuracoes: configuracaoIcon,
+  logout: logoutIcon,
 } as const;
