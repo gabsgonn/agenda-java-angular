ddd97e19e2daa85abcb05dba37499ccc8536bd2e
gabriel.goncalves <gab.gd23@gmail.com>
2026-07-29 12:18:27 -0300

    Finalizando estilização sidenav

--- frontend/agenda/src/app/shared/icons/raw-icons.ts
+++ frontend/agenda/src/app/shared/icons/raw-icons.ts
@@ -2,10 +2,12 @@ import { tarefaIcon } from './tarefas-list.icon';
 import { contatoIcon } from './contatos-list.icon';
 import { configuracaoIcon } from './configuracao.icon';
 import { logoutIcon } from './logout.icon';
+import { plusIcon } from './plus.icon';
 
 export const RawIcons = {
   tarefas: tarefaIcon,
   contatos: contatoIcon,
   configuracoes: configuracaoIcon,
   logout: logoutIcon,
+  plus: plusIcon,
 } as const;
