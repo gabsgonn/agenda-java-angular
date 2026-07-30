a667eb2017b03b370eed8b8924835d566b6c66e3
gabriel.goncalves <gab.gd23@gmail.com>
2026-07-30 15:58:37 -0300

    Criação tela de Configurações do usuário

--- frontend/agenda/src/app/app.routes.ts
+++ frontend/agenda/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { TarefaList } from './pages/tarefas/tarefa-list/tarefa-list';
 import { TarefaForm } from './pages/tarefas/tarefa-form/tarefa-form';
+import { ContatoList } from './pages/contatos/contato-list/contato-list';
+import { UserSettings } from './features/user-settings/user-settings';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'tarefas', pathMatch: 'full' },
   { path: 'tarefas', component: TarefaList },
   { path: 'tarefas/formulario', component: TarefaForm },
+  { path: 'contatos', component: ContatoList },
+  { path: 'configuracoes', component: UserSettings },
 ];
