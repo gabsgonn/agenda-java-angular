d0c8fe9bad5e13e033b29228d7bb363df1bf7a00
gabriel.goncalves <gab.gd23@gmail.com>
2026-07-31 14:26:37 -0300

    Criando query para filtrar busca de tarefas

---
+++ backend/agenda/src/main/java/com/gabrielgon/agenda/domain/enums/FiltroPeriodo.java
@@ -0,0 +1,7 @@
+package com.gabrielgon.agenda.domain.enums;
+
+public enum FiltroPeriodo {
+    HOJE,
+    AMANHA,
+    ESTA_SEMANA,
+}
--- backend/agenda/src/main/java/com/gabrielgon/agenda/repository/TarefaRepository.java
+++ backend/agenda/src/main/java/com/gabrielgon/agenda/repository/TarefaRepository.java
@@ -1,7 +1,28 @@
 package com.gabrielgon.agenda.repository;
 
 import com.gabrielgon.agenda.domain.Tarefa;
+import com.gabrielgon.agenda.domain.enums.Prioridade;
+import com.gabrielgon.agenda.domain.enums.StatusTarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    @Query("SELECT t FROM Tarefa t WHERE " +
+            "(:busca IS NULL OR LOWER(t.titulo) LIKE LOWER(CONCAT('%', :busca, '%'))) AND " +
+            "(:status IS NULL OR t.status IN :status) AND " +
+            "(:prioridade IS NULL OR t.prioridade IN :prioridade) AND " +
+            "(CAST(:dataInicio AS date) IS NULL OR t.dataVencimento >= :dataInicio) AND " +
+            "(CAST(:dataFim AS date) IS NULL OR t.dataVencimento <= :dataFim)")
+    List<Tarefa> buscarComFiltrosDinamicamente(
+            @Param("busca") String busca,
+            @Param("status") List<StatusTarefa> status,
+            @Param("prioridade") List<Prioridade> prioridade,
+            @Param("dataInicio")LocalDate dataInicio,
+            @Param("dataFim") String dataFim
+            );
 }
